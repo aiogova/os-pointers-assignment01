83e6349ffc034e594f19f544241be82ac77b0892
Anastasia Iogova <aiogova77@gmail.com>
2026-02-12 09:56:54 -0600

    Got basic prompts working. Still need to do calculateStudentAverage and advanced data validation.

--- pointers.cpp
+++ pointers.cpp
@@ -21,7 +21,59 @@ int main(int argc, char **argv)
 
     // Sequence of user input -> store in fields of `student`
 
+    // get student ID
+    std::string userIDPrompt = "Please enter the student's id number: ";
+    int minStudentID = 0;
+    int maxStudentID = 999999999;
+    student.id = promptInt(userIDPrompt, minStudentID, maxStudentID);
+
+    //std::cout << "Your student user ID is: " << student.id << std::endl;
+
+
+    // get student first name
+    student.f_name = new char[128];
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> student.f_name;
+
+    //std::cout << "Your first name is: " << student.f_name << std::endl;
+
+    // get student last name
+    student.l_name = new char[128];
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> student.l_name;
+
+    // get student's number of assignments
+    std::string numAssignmentsPrompt = "Please enter how many assignments were graded: ";
+    int minNumAssignments = 1;
+    int maxNumAssignments = 134217728;
+    student.n_assignments = promptInt(numAssignmentsPrompt, minNumAssignments, maxNumAssignments);
+    
+    //std::cout << "Your number of assignments is: " << student.n_assignments << std::endl;
+
+    // get student's assignment grades
+    student.grades = new double[student.n_assignments];
+    double minGrade = 0;
+    double maxGrade = 999.000000;
+    std::string gradePrompt;
+
+    for (int i = 0; i < student.n_assignments; i++) {
+        gradePrompt = "Please enter grade for assignment " + std::to_string(i) + ": ";
+
+        student.grades[i] = promptDouble(gradePrompt, minGrade, maxGrade);
+    }
+
+    //  for (int i = 0; i < student.n_assignments; i++) {
+    //     std::cout << gradePrompt << i << ": ";
+    //     std::cin >> student.grades[i];
+    // }
+    
+    //student.grades = promptDouble(gradePrompt, minGrade, maxGrade);
+    
+
     // Call `CalculateStudentAverage(???, ???)`
+    // double *avgPtr = &average;
+    calculateStudentAverage(student.grades, &average);
+
     // Output `average`
 
     return 0;
@@ -35,6 +87,10 @@ int main(int argc, char **argv)
 int promptInt(std::string message, int min, int max)
 {
     // Code to prompt user for an int
+    int result;
+    std::cout << message;
+    std::cin >> result;
+    return result;
 }
 
 /*
@@ -45,6 +101,10 @@ int promptInt(std::string message, int min, int max)
 double promptDouble(std::string message, double min, double max)
 {
     // Code to prompt user for a double
+    double result;
+    std::cout << message;
+    std::cin >> result;
+    return result;
 }
 
 /*
@@ -54,4 +114,9 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    double result = 0.0;
+    // for (int i = 0; i < student.n_assignments; i++) {
+
+    // }
+    *avg = result;
 }
